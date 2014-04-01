97d1b20809399a61327bb74ae430e28c5054ad4d
Danielle Hata <dhata@usc.edu>
2014-04-01 16:13:52 -0700

    starting the php pages

---
+++ src/php/index.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+<div id="container">
+	<form method="post" action="." id="login">
+
+                <label for="username">Username:</label><input type="text" name="username" value="username" class="text" id="username"/>
+                <label for="password">Password:</label><input type="password" name="password" value="password" id="password" class="text"/>
+                <label for="remember">Remember user?</label><input type="checkbox" name="remember" id="remember" class="checkbox"/>
+                <input type="submit" class="button" value="GO" id="submit"/>
+
+            </form>
+</div>
+ </body>
+</html>
