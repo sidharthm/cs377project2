371d50f35a1ffab229e391f9abc54e2495a9c7f2
Roy Zheng <roycraft3@gmail.com>
2014-04-03 16:59:32 -0700

    adding bootstraP

--- src/flask/index.py
+++ src/flask/index.py
@@ -1,5 +1,19 @@
-from flask import Flask
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+DATABASE=os.path.join(app.root_path,'flaskr.db'),
+DEBUG=True,
+SECRET_KEY='development key',
+USERNAME='admin',
+PASSWORD='default'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS',silent=True);
 
 @app.route('/')
 def hello_world():
@@ -8,6 +22,11 @@ def hello_world():
 @app.route('/registration')
 def registration():
     return 'Registration'
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory=sqlite3.Row
+    return rv
 
 if __name__ == '__main__':
     app.run(debug=True)
