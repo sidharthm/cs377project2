780c2b4e50df54c30f4fa9be5168000297bfb8ba
Roy Zheng <roycraft3@gmail.com>
2014-04-03 16:48:15 -0700

    setting up user accounts system

--- src/flask/index.py
+++ src/flask/index.py
@@ -2,9 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
     return 'Hello World!'
+
+@app.route('/registration')
+def registration():
+    return 'Registration'
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/flask/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+id integer primary key autoincrement,
+username text not null,
+password text not null
+);
