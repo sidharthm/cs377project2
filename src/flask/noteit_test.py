d5e9a38d2b9417010c1896a9ad9cfb07c8811627
sidharthm <sidhartm@usc.edu>
2014-04-08 16:00:52 -0700

    some small changes

--- src/flask/noteit_test.py
+++ src/flask/noteit_test.py
@@ -20,5 +20,25 @@ class indexTestCase(unittest.TestCase):
         assert 'Home' in rv.data
         assert 'Log-In' in rv.data
         assert 'Register' in rv.data
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.login('admin', 'default')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+        rv = self.login('adminx', 'default')
+        assert 'Invalid username' in rv.data
+        rv = self.login('admin', 'defaultx')
+        assert 'Invalid password' in rv.data
+        
 if __name__ == '__main__':
     unittest.main()
